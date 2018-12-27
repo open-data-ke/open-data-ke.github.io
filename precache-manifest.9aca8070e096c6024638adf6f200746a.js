fa61d3afa894e97deb6ad8cf9c2d5cbc95fd0376
Brian Mugweru <brian@ralphowino.com>
2018-12-28 02:43:01 +0300

    Updates

---
+++ precache-manifest.9aca8070e096c6024638adf6f200746a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bd545257a68246e296d1",
+    "url": "/static/js/main.bd545257.chunk.js"
+  },
+  {
+    "revision": "b2b0790b5efda9ff16ab",
+    "url": "/static/js/1.b2b0790b.chunk.js"
+  },
+  {
+    "revision": "bd545257a68246e296d1",
+    "url": "/static/css/main.e6710cfb.chunk.css"
+  },
+  {
+    "revision": "29414564cfa585c497a0878cd8c973d7",
+    "url": "/index.html"
+  }
+];
